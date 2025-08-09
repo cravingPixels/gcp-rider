442918d550c45411e1c26f2c304eedd07a754113
cravingPixels <4791819+cravingPixels@users.noreply.github.com>
2025-08-09 12:10:10 +0200

    feat: bootstrap project

---
+++ main_test.go
@@ -0,0 +1,4 @@
+package main
+
+// Unit testing for fetchInstances is complex due to the nature of the GCP client library.
+// A full integration test against a real GCP project would be the best way to test this functionality.
